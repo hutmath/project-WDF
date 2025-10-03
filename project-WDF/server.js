99eeee50f230b73ab135f09c11d6c0a1975cf26e
yuli24of <yuli24of@student.ju.se>
2025-10-03 14:08:48 +0200

    start on database and server

--- project-WDF/server.js
+++ project-WDF/server.js
@@ -8,55 +8,38 @@ Administrator login: admin
 Administrator password: xxxx
 */
 
-// --- LOAD THE PACKAGES
-const express=require('express')
-const {engine}=require('express-handlebars') // load the ahndlebars package for express
-
-// --- DEFINE VARIABLES AND CONSTANTS
-const port=8080
+// --- SETUP SERVER
+const express=require('express') // load express package
+// define variables and constants
 const app=express()
-// here or below??? in connect to database?
-const sqlite3=require('sqlite3') // read the sqlite3 package
+const port=8080
 
-// --- DEFINE MIDDLEWARES
+// --- HANDLEBARS
+const {engine}=require('express-handlebars') // load handlebars package (for express)
+// setup handlebars // define middlewares
 app.use(express.static('public'))
 app.engine('handlebars', engine()) // initialize the engine to be handlebars
 app.set('view engine', 'handlebars') // set handlebars as the view engine
 app.set('views', './views') // define the views directory to be ./views
 
-// --- CONNECT TO DATABASE
-/* I don't know where to have these, if they should be where they are now, or in here
-const sqlite3=require('sqlite3') // read the sqlite3 package ????
-or
+// --- SQLITE3
+const sqlite3=require('sqlite3') // read the sqlite3 package
+// connect to database
 const dbFile='my-project-db.sqlite3.db'
-*/
+db = new sqlite3.Database(dbFile)
 
-// --- DEFINE THE ROUTES AND
+
+// --- DEFINE THE ROUTES AND DEFAULT ROUTE
 // define the default '/' ROUTE
 app.get('/', (request, response) => {
     // res.render ('home.handlebars')
     response.render('home')
 })
 
-app.get('/books', (req, res) => {
-    res.render('books.handlebars')
-})
-// OR
-/*
-
-app.get('/', (request, response) => {
-    // res.render ('home.handlebars')
-    response.render('home.handlebars') // here's the change!
-})
-
-*/
-
-
-// here or above??? in connect to database?
-const dbFile='my-project-db.sqlite3.db'
-
-db = new sqlite3.Database(dbFile)
-
 app.listen(port, () => {
     console.log(`Server up and running on http://localhost:${port}...`)
+})
+
+app.get('/books', (req, res) => {
+    res.render('books.handlebars')
 })
