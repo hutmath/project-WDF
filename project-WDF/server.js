794555afc5f7f7e0c73df5e2504e269dbfe77182
yuli24of <yuli24of@student.ju.se>
2025-10-09 11:30:47 +0200

    login done for grade 4, css for some pages

--- project-WDF/server.js
+++ project-WDF/server.js
@@ -14,28 +14,33 @@ const {engine}=require('express-handlebars') // load handlebars package
 const sqlite3=require('sqlite3'); // read the sqlite3 package
 const session = require('express-session');
 const connectSqlite3 = require('connect-sqlite3');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3000;
-const adminPassword='$2b$12$ERkocBkyahF2C2A5AfTn1urDFO08qSS1TnA1FYcTJ6o4opqhzm0.e';
-const bcrypt = require('bcrypt');
 
-// setup handlebars // define middlewares
+const adminPassword='$2b$12$VlMqUd/bZkT9oDf20pEt7evv1si.TAPbs53kmEa90DSn0uLxMNYk2';
+
+
+
 app.use(express.static('public'));
-// ADDED BECAUSE THERE IS AN ERROR
+
+// FOR THE LIGIN FORM
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
 // HANDLEBARS
-app.engine('handlebars', engine()); // initialize the engine to be handlebars
-app.set('view engine', 'handlebars'); // set handlebars as the view engine
-app.set('views', './views');// define the views directory to be ./views
+app.engine('handlebars', engine()); 
+app.set('view engine', 'handlebars'); 
+app.set('views', './views');
 
 
 // connect to DATABASE
 const dbFile='my-project-db.sqlite3.db';
- db = new sqlite3.Database(dbFile);
+db = new sqlite3.Database(dbFile);
+//  STORE SESSION
 const SQLiteStore = connectSqlite3(session);
+// DEFINE THE SESSION
 app.use (session ({
     store: new SQLiteStore ({ db: "session db.db"}),
     "saveUninitialized": false,
@@ -48,7 +53,7 @@ app.use((request,response,next) =>{
     response.locals.session = request.session
     next()
 })
-// LOGIN PAGE
+// LOGIN FORM
 app.get('/login', (request,response) => {
     response.render ('login')
 })
@@ -57,33 +62,65 @@ app.get('/login', (request,response) => {
  // LOGIN PROCESSING
 app.post('/login', (request, response) => {
     console.log(`Here comes the data received from the form on the client: ${request.body.un} - ${request.body.pw}`);
+    db.get('SELECT * FROM Member WHERE username=?', [request.body.un], (err,theUser) => {
+        console.log(JSON.stringify(theUser))
+        if(err) {
+            console.log('Error in login');
+            const model = { error: "Error in login." };
+            return response.render('login', model);
+        } else if (typeof theUser !== 'undefined') {
+            bcrypt.compare(request.body.pw, theUser.Password, (err, result) => {
+                if (err) {
+                    console.log('Error in password comparison');
+                    const model = { error: "Error in password comparison." };
+                    response.render('login', model);
+                }
 
-    if (request.body.un === "admin") {
-        bcrypt.compare(request.body.pw, adminPassword, (err, result) => {
-            if (err) {
-                console.log('Error in password comparison');
-                const model = { error: "Error in password comparison." };
-                return response.render('login', model);
-            }
+                if (result) {
+                    request.session.isLoggedIn = true;
+                    request.session.un = theUser.username;
+                    request.session.isAdmin = (theUser.username=='admin')
 
-            if (result) {
-                request.session.isLoggedIn = true;
-                request.session.un = request.body.un;
-                request.session.isAdmin = true;
+                    console.log('> SESSION INFORMATION:', JSON.stringify(request.session));
+                response.render('loggedin');
+                } else {
+                    console.log('Wrong password');
+                    const model = { error: "Wrong password! Please try again." };
+                    response.render('login', model);
+                }
+            });
+        } else {
+            console.log('Error in login');
+            const model = { error: "Error in login." };
+            response.render('login', model);
+        }
+    })
+    // if (request.body.un === "admin") {
+    //     bcrypt.compare(request.body.pw, adminPassword, (err, result) => {
+    //         if (err) {
+    //             console.log('Error in password comparison');
+    //             const model = { error: "Error in password comparison." };
+    //             return response.render('login', model);
+    //         }
 
-                console.log('> SESSION INFORMATION:', JSON.stringify(request.session));
-                return response.render('loggedin');
-            } else {
-                console.log('Wrong password');
-                const model = { error: "Wrong password! Please try again." };
-                return response.render('login', model);
-            }
-        });
-    } else {
-        console.log('Wrong username');
-        const model = { error: "Wrong username! Please try again." };
-        return response.render('login', model);
-    }
+    //         if (result) {
+    //             request.session.isLoggedIn = true;
+    //             request.session.un = request.body.un;
+    //             request.session.isAdmin = true;
+
+    //             console.log('> SESSION INFORMATION:', JSON.stringify(request.session));
+    //          response.render('loggedin');
+    //         } else {
+    //             console.log('Wrong password');
+    //             const model = { error: "Wrong password! Please try again." };
+    //          response.render('login', model);
+    //         }
+    //     });
+    // } else {
+    //     console.log('Wrong username');
+    //     model = { error: "Wrong username! Please try again." };
+    //     response.render('login', model);
+    // }
 });
 
 // LOGOUT PROCESSING
@@ -100,9 +137,6 @@ app.get('/logout', (req,res) => {
     })
 })
 
-
-
- 
 
 // --- DEFINE THE ROUTES AND DEFAULT ROUTE
 // define the DEFAULT '/' ROUTE
@@ -124,9 +158,6 @@ app.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
 app.get('/my_pages', (req, res) => {
     res.render('my_pages')
 })
